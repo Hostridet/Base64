003cf734499a986c5c81942327a4c1b144650ad0
Никита Румянцев <rumiantcev.no@students.dvfu.ru>
2021-10-25 12:18:21 +1000

    done comit

--- cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/host/CLionProjects/base64/googletest
-# Build directory: /home/host/CLionProjects/base64/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/host/CLionProjects/base64/googletest/googletest
-# Build directory: /home/host/CLionProjects/base64/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
